5363842c49bb62d578aa03735a0cb8d2aa4185e6
yuwen <zengyuwen@dianmi365.com>
2017-05-04 16:08:19 +0800

    修正update脚本

--- src/update.js
+++ src/update.js
@@ -18,11 +18,18 @@ let updateJson = JSON.parse(updateRs);
 let updatePkg = [];
 for (let i in updateJson) {
     let version = updateJson[i].wanted;
-    if (version == 'git') {
+    if (version == updateJson[i].current) {
+        //is update to date
+    } else if (version == 'git') {
         updatePkg.push(i);
     } else {
         updatePkg.push(`${i}@${version}`);
     }
+}
+
+if (updatePkg.length == 0) {
+    console.log(chalk.green('Already up-to-date!'));
+    process.exit(0)
 }
 
 shell.exec(`npm i ${updatePkg.join(' ')} --registry=${config.registry}`)
