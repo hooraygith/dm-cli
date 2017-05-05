21fb2f785265b464799f4f42cbf5f847431b42bf
yuwen <zengyuwen@dianmi365.com>
2017-05-05 17:35:20 +0800

    增加build钩子

--- src/dm-build.js
+++ src/dm-build.js
@@ -4,6 +4,8 @@ const shell = require('shelljs');
 const config = require('./config/index.js');
 const chalk = require('chalk');
 const program = require('commander');
+const _DIR = process.cwd();
+
 
 program
     .option('-E, --env [env]', '要编译的环境')
@@ -14,26 +16,37 @@ if (!program.env) {
     process.exit(1);
 }
 
-if (!shell.test('-f', process.cwd() + "/build/webpack.config." + program.env + ".js")) {
-    console.log(chalk.red('找不到文件：' + process.cwd() + "/build/webpack.config." + program.env + ".js"));
+if (!shell.test('-f', `${_DIR}/build/webpack.config.${program.env}.js`)) {
+    console.log(chalk.red(`找不到文件：${_DIR}/build/webpack.config.${program.env}.js`));
     process.exit(1);
 }
 
 console.log(chalk.yellow('============compile============'))
 
-//todo compile前置钩子
+//compile前置钩子
+if (!shell.test('-f', `${_DIR}/build/build-pre.js`)) {
+    shell.exec(`node ${_DIR}/build/build-pre.js`, {
+        silent: true
+    });
+}
 
 const webpack = require("webpack");
-const webpackConfig = require(process.cwd() + "/build/webpack.config." + program.env + ".js");
+const webpackConfig = require(_DIR + "/build/webpack.config." + program.env + ".js");
 
 // returns a Compiler instance
 const compiler = webpack(webpackConfig);
 
 compiler.run((err, stats) => {
     console.log(stats.toString({
-            chunks: false, // Makes the build much quieter
-            colors: true
-        }))
-        // todo compile后置钩子
+        chunks: false, // Makes the build much quieter
+        colors: true
+    }))
+
+    // compile 后置钩子
+    if (!shell.test('-f', `${_DIR}/build/build-post.js`)) {
+        shell.exec(`node ${_DIR}/build/build-post.js`, {
+            silent: true
+        });
+    }
     console.log(chalk.yellow('============end============'))
 });
