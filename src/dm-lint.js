68fa28c1939da27a8e8d5291911047f6f6754e38
yuwen <zengyuwen@dianmi365.com>
2017-05-09 16:28:02 +0800

    add publish env & add lint --commit

--- src/dm-lint.js
+++ src/dm-lint.js
@@ -6,6 +6,14 @@ const chalk = require('chalk')
 const program = require('commander')
 const _DIR = process.cwd()
 
+program
+    .option('-C, --commit','修复后是否需要提交')
+    .parse(process.argv)
+
 if (shell.test('-f', `${_DIR}/src`)) { // 当没有src目录时 vuefix会报错，加个判断修复
     shell.exec('vuefix -d src')
+
+    if(program.commit){
+        shell.exec('git add -A')
+    }
 }
