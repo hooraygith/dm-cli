346394f5c85e7d67128439ae9506dd50c0f6c0ca
yuwen <zengyuwen@dianmi365.com>
2017-05-12 16:06:56 +0800

    fix

--- scripts/install.js
+++ scripts/install.js
@@ -1,7 +1,9 @@
 const shell = require('shelljs')
 
 const dep = {
-  // 'stylelint-scss': '1.4.4',
+  'stylelint': '7.10.1',
+  'stylelint-scss': '1.4.4',
+  'eslint': '3.19.0',
   'eslint-config-dianmife': '0.1.4',
   'eslint-config-standard': '10.2.1',
   'eslint-plugin-standard': '3.0.1',
