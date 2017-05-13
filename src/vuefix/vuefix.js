736e1d8bc1d6d9e55f1b0455313ae5b9c9abd38e
yuwen <zengyuwen@dianmi365.com>
2017-05-13 15:19:36 +0800

    fix

--- src/vuefix/vuefix.js
+++ src/vuefix/vuefix.js
@@ -31,7 +31,7 @@ module.exports = module.exports.default = function (files) {
               console.log(data.output)
               process.exit(1)
             } else {
-              node.childNodes[0].value = '\n' + result.css
+              node.childNodes[0].value = '\n' + data.css
               fs.writeFileSync(filePath, parser.serialize(fragment))
             }
           })
