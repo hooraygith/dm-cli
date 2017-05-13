816ada56e8252fd18235b9cae253472293bf3b90
yuwen <zengyuwen@dianmi365.com>
2017-05-13 16:02:33 +0800

    fix

--- src/vuefix/vuefix.js
+++ src/vuefix/vuefix.js
@@ -6,27 +6,29 @@ const parser = require('parse5')
 const eslintfixer = require('./eslintfixer.js')
 const fs = require('fs')
 
-module.exports = module.exports.default = function (files) {
-  const vuefiles = files.filter(f => f.match(/\.(vue|js|scss)$/gi))
+module.exports = module.exports.default = function(files) {
+    const jsfiles = files.filter(f => f.match(/\.(js)$/gi))
+    const vuefiles = files.filter(f => f.match(/\.(vue)$/gi))
 
-  vuefiles.forEach((filePath) => {
-    const fileContent = fs.readFileSync(filePath, 'utf-8')
+    jsfiles.forEach((filePath) => {
+        const fileContent = fs.readFileSync(filePath, 'utf-8')
+        fs.writeFileSync(filePath, eslintfixer(fileContent))
+        console.log(`${filePath} has formated`)
+    })
 
-    if (filePath.match(/\.js/i)) {
-      fs.writeFileSync(filePath, eslintfixer(fileContent))
-    } else if (filePath.match(/\.vue/i)) {
-      const fragment = parser.parseFragment(fileContent)
-      const childNodes = fragment.childNodes
-      for (let node of childNodes) {
-        if (node.nodeName === 'script') {
-          const scriptString = parser.serialize(node)
-          node.childNodes[0].value = eslintfixer(scriptString)
-          fs.writeFileSync(filePath, parser.serialize(fragment))
+    vuefiles.forEach((filePath) => {
+        const fileContent = fs.readFileSync(filePath, 'utf-8')
+        const fragment = parser.parseFragment(fileContent)
+        const childNodes = fragment.childNodes
+        for (let node of childNodes) {
+            if (node.nodeName === 'script') {
+                const scriptString = parser.serialize(node)
+                node.childNodes[0].value = eslintfixer(scriptString)
+                fs.writeFileSync(filePath, parser.serialize(fragment))
+                console.log(`${filePath} has formated`)
+            }
         }
-      }
-    }
-    console.log(`${filePath} has formated`)
-  })
+    })
 
-  cmd.exec(`eslint ${vuefiles.join(' ')}`)
+    cmd.exec(`eslint ${jsfiles.join(' ')} ${vuefiles.join(' ')}`)
 }
