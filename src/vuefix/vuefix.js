cc26ea059faac7f6df50df2297fb580fd6d88958
yuwen <zengyuwen@dianmi365.com>
2017-05-13 15:52:57 +0800

    fix

--- src/vuefix/vuefix.js
+++ src/vuefix/vuefix.js
@@ -4,7 +4,6 @@
 const cmd = require('../util/cmd.js')
 const parser = require('parse5')
 const eslintfixer = require('./eslintfixer.js')
-const stylelintfixer = require('./stylelintfixer.js')
 const fs = require('fs')
 
 module.exports = module.exports.default = function (files) {
@@ -23,18 +22,6 @@ module.exports = module.exports.default = function (files) {
           const scriptString = parser.serialize(node)
           node.childNodes[0].value = eslintfixer(scriptString)
           fs.writeFileSync(filePath, parser.serialize(fragment))
-        } else if (node.nodeName === 'style') {
-          const styleString = parser.serialize(node)
-
-          stylelintfixer(styleString).then((data) => {
-            if (data.errored) {
-              console.log(data.output)
-              process.exit(1)
-            } else {
-              node.childNodes[0].value = data.results
-              fs.writeFileSync(filePath, parser.serialize(fragment))
-            }
-          })
         }
       }
     }
